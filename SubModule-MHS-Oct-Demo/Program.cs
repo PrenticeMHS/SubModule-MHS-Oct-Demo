b767913a893d12e8d75f6d3f8a67dd5c1dd37300
Prentice Lauzon <prentice.lauzon@mhs.com>
2025-10-15 22:54:50 -0400

    Add database connection and query functionality
    Updated App.config with SQL Server connection string and app settings. Modified Program.cs to establish a database connection and execute a query to retrieve and print records from the Product table. Added Microsoft.Data.SqlClient package reference in the project file.

--- SubModule-MHS-Oct-Demo/Program.cs
+++ SubModule-MHS-Oct-Demo/Program.cs
@@ -1,15 +1,43 @@
 ﻿using DbupDemo;
 using System.Data;
+using Microsoft.Data.SqlClient;
+
 namespace SubModuleDemo
 {
     class Program
     {
         public static int Main(string[] args)
         {
-            DbupDemo.Program.updater();
-            Console.WriteLine("Hello, World!");
+            //TODO: ADD DBUPDemo updator
+            
+
+            // Get connection string from config
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Query all stores from the Stores table
+                var command = new SqlCommand("SELECT * FROM Product", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    // Print column names
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write($"{reader.GetName(i)}\t");
+                    Console.WriteLine();
+
+                    // Print each store row
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            Console.Write($"{reader.GetValue(i)}\t");
+                        Console.WriteLine();
+                    }
+                }
+            }
+
             return 1;
         }
     }
 }
-
